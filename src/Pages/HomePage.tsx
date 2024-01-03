2a41788ed3a9601b4704b5600cf66b3e46c85e0e
Supul Kalhara <supulkalhara7@gmail.com>
2024-01-03 21:08:37 +0530

    initial commit

---
+++ src/Pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroComponent from '../Components/Home/HeroComponent'
+import TestimonialComponent from '../Components/Home/TestimonialComponent'
+
+function HomePage() {
+  return (
+    <div>
+      <HeroComponent />
+      <TestimonialComponent />
+    </div>
+  )
+}
+
+export default HomePage
